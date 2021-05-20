eb7bca3f3d87d42ae237e731e31fcca7605cfe5f
Shubham Jha <whiteshadow221998@gmail.com>
2021-05-20 13:05:49 +0530

    Done

---
+++ Project1/src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Hello Hello");
+        System.out.println(10+12);
+    }
+}
